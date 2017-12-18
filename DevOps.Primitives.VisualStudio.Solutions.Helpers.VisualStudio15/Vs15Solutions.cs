942ea9c551c720e2642b9dc497fddb9683985813
Christopher Dorst <chris@dorst.email>
2017-12-18 17:54:52 -0600

    Adding Vs15Solutions.Create helpers

---
+++ DevOps.Primitives.VisualStudio.Solutions.Helpers.VisualStudio15/Vs15Solutions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DevOps.Primitives.VisualStudio.Solutions.Helpers.VisualStudio15
+{
+    public static class Vs15Solutions
+    {
+        public static Solution Create(
+            string name,
+            IEnumerable<SolutionProject> sourceProjects,
+            IEnumerable<SolutionProject> testsProjects)
+            => Common.Solutions.Create(
+                name,
+                Vs15VersionInfo.Create(),
+                sourceProjects,
+                testsProjects);
+    }
+}
---
+++ DevOps.Primitives.VisualStudio.Solutions.Helpers.VisualStudio15/Vs15VersionInfo.cs
@@ -0,0 +1,13 @@
+﻿using DevOps.Primitives.VisualStudio.Solutions.Helpers.Common;
+
+namespace DevOps.Primitives.VisualStudio.Solutions.Helpers.VisualStudio15
+{
+    public static class Vs15VersionInfo
+    {
+        public const string VersionComment = "Visual Studio 15";
+        public const string VisualStudioVersion = "15.0.27130.2010";
+
+        public static VisualStudioVersionInfo Create()
+            => new VisualStudioVersionInfo(VersionComment, VisualStudioVersion);
+    }
+}
